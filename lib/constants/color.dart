feff59322d2b45d9ffa9ac9e5b65f22ab513ac69
Mokhtari Hadj Mohamed <7adj.mo7amed@gmail.com>
2025-08-29 21:20:42 +0100

    Initial commit

---
+++ lib/constants/color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+
+
+const Color primery = Color(0xffF1EDFE);
+const Color secondary = Color(0xff8195F6);
+const Color tertiary = Color(0xff2B4490);
+const Color red = Color(0xffDA0000);
